105fecca28047173f39d66e1b2f6f1a908f1c408
pm1017 <minpan1206@gmail.com>
2025-07-15 19:18:52 +0800

    Initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,78 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const licenseLink = 'Content licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a>. '
+const docusaurusLink = 'Built with <a href="https://docusaurus.io/" target="_blank" rel="noopener noreferrer">Docusaurus</a>.'
+
+const config: Config = {
+  title: 'panmin',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  url: 'https://panmin.me',
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  plugins: ['docusaurus-plugin-sass'],
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: false,
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.scss',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        { to: '/blog', label: 'Blog', position: 'right' },
+        {
+          label: 'GitHub',
+          position: 'right',
+          href: 'https://github.com/pm1017',
+        },
+      ],
+    },
+    footer: {
+      copyright: `Copyright © ${new Date().getFullYear()} Martin Pan. All rights reserved. <br/>${licenseLink}${docusaurusLink}`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import type { ReactNode } from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import style from './index.module.scss';
+
+export default function Home(): ReactNode {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`${siteConfig.title}`}>
+      <main>
+        <section className={style.home}>
+          <h1>Hello.</h1>
+          <h2>My name is Martin Pan.</h2>
+          <p>
+            I am a front-end engineer. Over the years, I have dedicated myself to enhancing my
+            skills in front-end development, accumulating a wealth of experience and expertise.
+            Throughout my career, I have been involved in the development and maintenance of various
+            projects, ranging from small websites to large, complex applications.
+          </p>
+          <p>
+            My technical stack includes, but is not limited to, HTML5, CSS3, JavaScript, Vue.js,
+            React, Node.js, and related front-end development tools and frameworks. I enjoy
+            exploring the latest front-end technologies and applying them to real-world projects to
+            improve development efficiency and user experience.
+          </p>
+          <p>
+            In my work, I focus on code quality and maintainability, and I excel in collaborating
+            with team members to solve problems and ensure project success. I believe that
+            continuous learning and practice are essential to staying competitive in a rapidly
+            changing technological landscape.
+          </p>
+          <p>
+            Besides my job, I also enjoy writing technical blogs to share my development experiences
+            and insights. If you are interested in front-end development, you can check out my{' '}
+            <a href="https://github.com/pm1017" target="_blank" rel="noopener noreferrer">
+              GitHub Profile
+            </a>
+            , where we can exchange ideas and learn together.
+          </p>
+        </section>
+      </main>
+    </Layout>
+  );
+}
